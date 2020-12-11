4490660f6727277ebe42d7cfda87f8f0a41c4bc1
yux521 <73996546+yux521@users.noreply.github.com>
2020-12-11 17:53:14 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
